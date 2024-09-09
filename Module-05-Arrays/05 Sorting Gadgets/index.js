4e05c7c49d2ff71e1222586ac70eb0ce1861a8d3
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-09 16:28:48 +0200

    Add filter and map

--- Module-05-Arrays/05 Sorting Gadgets/index.js
+++ Module-05-Arrays/05 Sorting Gadgets/index.js
@@ -9,7 +9,12 @@ const mixedGadgets = [
 
 // Use filter method to create two arrays, one for Batman's gadgets and one for CatWoman's
 // Hint: Use filter to filter objects based on the 'owner' property
-
+const batmanGadgets = mixedGadgets.filter(gadget => gadget.owner === 'Batman');
+const catwomanGadgets = mixedGadgets.filter(gadget => gadget.owner === 'CatWoman');
 // Use map method to transform arrays into arrays of gadget names (strings)
 // Hint: Use map to extract the 'name' property from each object
+const batmanGadgetNames = batmanGadgets.map(gadget => gadget.name);
+const catwomanGadgetNames = catwomanGadgets.map(gadget => gadget.name);
 
+console.log(batmanGadgetNames);
+console.log(catwomanGadgetNames);
