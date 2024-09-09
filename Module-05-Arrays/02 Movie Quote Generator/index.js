3bb97debbc573648b1d989f9d7b8766c88074420
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-09 11:31:27 +0200

    Move script to head tag

--- Module-05-Arrays/02 Movie Quote Generator/index.js
+++ Module-05-Arrays/02 Movie Quote Generator/index.js
@@ -29,25 +29,25 @@ function getRandomMovieQuote() {
     ];
 
     // Generate a random index to select a random quote
-
+    const randomIndex = Math.floor(Math.random() * quotes.length)
 
     // Return the selected random quote
-
+    return quotes[randomIndex]
 }
 
 // Function to log movie attributes and a random quote
 function randomMovieQuote(movieArray) {
     // Loop through the movieArray
-
+    for (const movie of movieArray) {
         // Destructure the movie object to extract title and year attributes
-    
+        const { title, year } = movie;
 
         // Call getRandomMovieQuote to get a random movie quote
-     
+        const quote = getRandomMovieQuote();
 
         // Log movie title, year, and the random quote
 
+        }
     }
-}
 
 // Call the randomMovieQuote function and pass the movies array as an argument to demonstrate the functionality
