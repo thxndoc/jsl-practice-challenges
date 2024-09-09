63670b6424af49c9982667437dfad56b6b979f55
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-09 10:09:57 +0200

    Move script to head tag

--- Module-05-Arrays/01 Array We Go/index.js
+++ Module-05-Arrays/01 Array We Go/index.js
@@ -7,19 +7,24 @@ let myFavoriteMovies = [
     "Avengers",
     "Black Panther",
     "Fantastic Beasts"
-];
+]; //remember that arrays are represented as strings
 
 // Step 2: Access the first and the last movie from your myFavoriteMovies array.
 let firstMovie = myFavoriteMovies[0];
-let lastMovie = myFavoriteMovies[4];
+let lastMovie = myFavoriteMovies[myFavoriteMovies.length - 1];
+// Break down of myFavoriteMovies.length - 1 step by step:
+// myFavoriteMovies.length: This part of the code gets the total number of items (movies) in the list called myFavoriteMovies. For example, if there are 5 movies in the list, myFavoriteMovies.length would give the value 5.
+// - 1: Since arrays in programming start counting from 0 (not 1), the last item in the list is at the position length - 1. If the total length is 5, the last movie is at position 4. So, subtracting 1 helps us find the correct position (or index) of the last movie.
+// In summary, myFavoriteMovies.length - 1 calculates the position of the last item in the list by taking the total number of items and subtracting one.
 
 // Display the first and the last movie on the web page.
 function displayMovies() {
     // Use document.getElementById to find the movies div.
-   
+   let movies = document.getElementById('movies')
 
     // Create and append the first movie title.
-
+    let firstMovieElement = document.createElement('p')
+    console.log(firstMovieElement)
 
     // Create and append the last movie title.
 
