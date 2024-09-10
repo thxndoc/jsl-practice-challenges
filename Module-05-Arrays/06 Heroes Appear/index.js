76e7acccb1345f5711aa65b64f1dff8d39f40e58
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-10 08:45:55 +0200

    Use join method to concatenate array elements separated by commas

--- Module-05-Arrays/06 Heroes Appear/index.js
+++ Module-05-Arrays/06 Heroes Appear/index.js
@@ -2,7 +2,7 @@ const movies = ["Justice League", "Wonder Woman", "Batman vs Superman"];
 
 // Use template literals to create a dynamic message
 // Hint: Utilize the `join` method to concatenate array elements with commas
-
+const movieList = `Wonder Woman appears in: ${movies.join(", ")}.`// movies.join - show all elements in the movies array, (", ") - make sure that they are separated by a comma and space.
 // Ensure a unique list of movies using Set
 // Hint: Initialize a Set using the array `movies` to automatically remove duplicates
 
