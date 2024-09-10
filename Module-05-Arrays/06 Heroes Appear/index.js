4f8ede90c25ebdd8be46e1390f4cc3f15d43e36b
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-10 08:50:28 +0200

    Initialize a Set and console log movieList and uniqueMovies

--- Module-05-Arrays/06 Heroes Appear/index.js
+++ Module-05-Arrays/06 Heroes Appear/index.js
@@ -2,8 +2,10 @@ const movies = ["Justice League", "Wonder Woman", "Batman vs Superman"];
 
 // Use template literals to create a dynamic message
 // Hint: Utilize the `join` method to concatenate array elements with commas
-const movieList = `Wonder Woman appears in: ${movies.join(", ")}.`// movies.join - show all elements in the movies array, (", ") - make sure that they are separated by a comma and space.
+const movieList = `Wonder Woman appears in: ${movies.join(", ")}.`;// movies.join - show all elements in the movies array, (", ") - make sure that they are separated by a comma and space.
 // Ensure a unique list of movies using Set
 // Hint: Initialize a Set using the array `movies` to automatically remove duplicates
-
+const uniqueMovies = new Set(movies);
 // Print the dynamic message and the unique list of movies
+console.log(movieList);
+console.log([...uniqueMovies]);
