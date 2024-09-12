6faf3a084cf43764614a413073b02946274fa75f
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-12 18:47:53 +0200

    Add solution

--- Module-06-Functions/05 Call Me Back/index.js
+++ Module-06-Functions/05 Call Me Back/index.js
@@ -1,9 +1,15 @@
 // Function to process pizza order
 function processOrder(order, callback) {
     // Check if the pizza name is "Unknown Pizza"
-    // If yes, call the callback with an error message
-    // If no, call the callback with null as the first argument and a success message as the second
-    
+    if (order.pizzaName === "Unknown pizza") {
+        // If yes, call the callback with an error message
+        callback("Sorry, we do not have that pizza")
+
+    } else {
+        // If no, call the callback with null as the first argument and a success message as the second
+        const successMessage = `Your order for ${order.pizzaName} has been received and is being prepared.`;
+        callback(null, successMessage);
+    }
     // Hint: Use an if-else statement to check the order.pizzaName
 }
 
