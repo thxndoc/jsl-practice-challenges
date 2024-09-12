01aa9ef7379dc84e59043c84189d80d06d3cf4fb
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-12 14:14:37 +0200

    Add solution

--- Module-06-Functions/02 Vis en Tjips/index.js
+++ Module-06-Functions/02 Vis en Tjips/index.js
@@ -1,8 +1,14 @@
 function orderFishAndChips() {
-    // Try to access 'fish' here
     console.log(fish); // What do you expect to happen?
     
-    // Now declare 'fish' at the bottom
     var fish = "Hake";
 }
 
+orderFishAndChips(); // Call the function
+
+// Expected Output Explanation:
+
+// The output will likely be `undefined`. 
+//This is because JavaScript hoists variable declarations to the top of their containing function or scope. 
+//In this case, the declaration `var fish` is hoisted to the top of the `orderFishAndChips` function, but its assignment (`fish = "Hake"`) remains at the bottom. 
+//As a result, when we try to `console.log` the value of `fish` before its assignment, it is considered declared but not yet assigned, so it holds the value `undefined`.
