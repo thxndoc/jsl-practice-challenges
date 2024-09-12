2db978a1990dbaab06a46308b6095072dfc88780
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-12 15:02:12 +0200

    Add solution

--- Module-06-Functions/03 Fries Feast/index.js
+++ Module-06-Functions/03 Fries Feast/index.js
@@ -8,14 +8,20 @@ function displayOrder(event) {
 
     // Accessing form elements
     // Hint: Retrieve the selected fries type and the topping (if provided)
-
+    const friesType = document.getElementById("friesType").value;
+    const topping = document.getElementById("topping").value.trim();//remove extra whitespace
+    const orderSummary = document.getElementById("orderSummary");
     // Constructing the order message
     // Challenge: Use template literals to create the order message
-
+    let orderMessage = `${baseOrder} ${friesType} fries`;
+    if (topping) {
+        orderMessage += ` with ${topping}`;
+    }
     // Displaying the order summary
     // Challenge: Set the text content of the "orderSummary" element to the order message
+    orderSummary.textContent = orderMessage;
 }
 
 // Adding event listener to the "Submit Order" button
 // Challenge: Add an event listener to the button with the id "submitOrder" and call the displayOrder function when clicked
-document.getElementById("submitOrder").addEventListener('click',displayOrder);
+document.getElementById("submitOrder").addEventListener("click", displayOrder);
