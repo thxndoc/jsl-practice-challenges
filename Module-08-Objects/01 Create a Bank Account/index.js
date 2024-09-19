93af202b0a7ed0fc7980a19236839329aa1a1b0e
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2024-09-19 14:11:35 +0200

    create object

--- Module-08-Objects/01 Create a Bank Account/index.js
+++ Module-08-Objects/01 Create a Bank Account/index.js
@@ -1,3 +1,11 @@
 // Let's create an object representing a bank account. This object will have properties for the account holder's name, account number, and balance. We'll also display its properties in the console.
+let bankAccount = {
+    holderName: "Thando",
+    accountNumber: 987890987,
+    balance: 50000000
+};
 
+console.log(`Account holder: ${bankAccount.holderName}`)
+console.log(`Account holder: ${bankAccount.accountNumber}`)
+console.log(`Account holder: R${bankAccount.balance}`)
 
